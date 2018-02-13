477308c13ed3fc6a2bf7ab77981153075c086092
Werberth Vinícius <werberthvinicius@gmail.com>
2018-02-12 23:16:39 -0300

    Adding example 2.7

---
+++ chapter 2/example_2_7.py
@@ -0,0 +1,19 @@
+lax_coordinates = (33.9425, -118.408056)
+city, year, pop, chg, area = ('Tokyo', 2003, 32450, 0.66, 8014)
+traveler_ids = [('USA', '31195885'), ('BRA', 'CE342567'), ('ESP', 'XDA205856')]
+
+for passport in sorted(traveler_ids):
+    print('%s/%s' % passport)
+
+# === result ===
+# BRA/CE342567
+# ESP/XDA205856
+# USA/31195885
+
+for country, _ in traveler_ids:
+    print(country)
+
+# === result ===
+# USA
+# BRA
+# ESP
